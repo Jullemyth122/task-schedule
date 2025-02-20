1dc55f3d55bf9fb75c82ebf6a1bda3b28358ce19
xenexashura125 <xenexashura@gmail.com>
2025-02-20 12:40:36 +0800

    Updated changes

--- src/utilities/firebase.js
+++ src/utilities/firebase.js
@@ -1,26 +1,22 @@
-// firebase.js
 import { initializeApp } from 'firebase/app';
-import { getAuth, GoogleAuthProvider, FacebookAuthProvider } from 'firebase/auth'; // Added Google and Facebook auth
+import { getAuth, GoogleAuthProvider, FacebookAuthProvider } from 'firebase/auth';
 import { getFirestore } from 'firebase/firestore';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBvH4dDT31I5Lq6CXFSNmDmpcXj__GPxBE",
-  authDomain: "task-scheduler-4b0ec.firebaseapp.com",
-  projectId: "task-scheduler-4b0ec",
-  storageBucket: "task-scheduler-4b0ec.appspot.com",
-  messagingSenderId: "125863140703",
-  appId: "1:125863140703:web:78edd27a41b71107b06aea",
-  measurementId: "G-NQV189QH05"
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID
 };
 
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
 const db = getFirestore(app);
 
-// Firebase providers for Google and Facebook
 const googleProvider = new GoogleAuthProvider();
 const facebookProvider = new FacebookAuthProvider();
-
-
 
 export { auth, db, googleProvider, facebookProvider };
